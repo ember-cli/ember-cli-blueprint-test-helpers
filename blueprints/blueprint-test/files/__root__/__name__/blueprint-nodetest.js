90d04ed6ce4e93a21dc3bd220107a57e29a38eef
Jake Bixby <trabus@gmail.com>
2015-10-07 22:29:01 -0700

    updated blueprint

--- blueprints/blueprint-test/files/__root__/__name__/blueprint-nodetest.js
+++ blueprints/blueprint-test/files/__root__/__name__/blueprint-nodetest.js
@@ -1,12 +1,20 @@
 'use strict';
 
-var setupTestHooks     = require('ember-cli-blueprint-test-helpers/lib/helpers/setup').setupTestHooks;
+var EOL                = require('os').EOL;
+var tmpenv             = require('ember-cli-blueprint-test-helpers/lib/helpers/tmp-env');
+var setupTestHooks     = require('ember-cli-blueprint-test-helpers/lib/helpers/setup');
 var BlueprintHelpers   = require('ember-cli-blueprint-test-helpers/lib/helpers/blueprint-helper');
 var generateAndDestroy = BlueprintHelpers.generateAndDestroy;
-var EOL                = require('os').EOL;
 
-describe('Acceptance: ember generate', function() {
-  setupTestHooks(this, 20000);
+describe('Acceptance: ember generate <%= blueprintName %>', function() {
+  setupTestHooks(this, 20000, tmpenv);;
   
+  it('<%= blueprintName %>', function() {
+    return generateAndDestroy(['<%= blueprintName %>', 'foo'], {
+      files: [
+        // { file: 'app/type/foo.js', contents: ['foo']}
+      ]
+    });
+  });
 
 });
