1464ba901e7e8fac7af4b90e1755f366b97c11b9
Jake Bixby <trabus@gmail.com>
2015-10-08 18:01:43 -0700

    added comments to blueprint

--- blueprints/blueprint-test/files/__root__/__name__/blueprint-nodetest.js
+++ blueprints/blueprint-test/files/__root__/__name__/blueprint-nodetest.js
@@ -6,11 +6,13 @@ var setupTestHooks     = require('ember-cli-blueprint-test-helpers/lib/helpers/s
 var BlueprintHelpers   = require('ember-cli-blueprint-test-helpers/lib/helpers/blueprint-helper');
 var generateAndDestroy = BlueprintHelpers.generateAndDestroy;
 
-describe('Acceptance: ember generate <%= blueprintName %>', function() {
+describe('Acceptance: ember generate and destroy <%= blueprintName %>', function() {
   setupTestHooks(this, 20000, tmpenv);;
   
-  it('<%= blueprintName %>', function() {
+  it('<%= blueprintName %> foo', function() {
+    // pass any additional command line options in the arguments array
     return generateAndDestroy(['<%= blueprintName %>', 'foo'], {
+      // define files to assert, and their contents
       files: [
         // { file: 'app/type/foo.js', contents: ['foo']}
       ]
