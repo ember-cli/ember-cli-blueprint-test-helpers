aaeac1813bf1997d6c69908e20c925c04bbbe6cb
Jake Bixby <trabus@gmail.com>
2015-11-17 02:35:22 -0800

    fixed uncaught promises causing false positives, added fixture packages to blueprint-nodetest blueprint

--- blueprints/blueprint-test/files/__root__/__name__/blueprint-nodetest.js
+++ blueprints/blueprint-test/files/__root__/__name__/blueprint-nodetest.js
@@ -1,13 +1,12 @@
 'use strict';
 
 var EOL                = require('os').EOL;
-var tmpenv             = require('ember-cli-blueprint-test-helpers/lib/helpers/tmp-env');
 var setupTestHooks     = require('ember-cli-blueprint-test-helpers/lib/helpers/setup');
 var BlueprintHelpers   = require('ember-cli-blueprint-test-helpers/lib/helpers/blueprint-helper');
 var generateAndDestroy = BlueprintHelpers.generateAndDestroy;
 
 describe('Acceptance: ember generate and destroy <%= blueprintName %>', function() {
-  setupTestHooks(this, tmpenv);
+  setupTestHooks(this);
   
   it('<%= blueprintName %> foo', function() {
     // pass any additional command line options in the arguments array
