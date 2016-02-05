0f45187f4a59a9b56091c4492880973d8da2f561
Jake Bixby <trabus@gmail.com>
2016-02-04 22:29:33 -0800

    fixed fixture path, bumped ember-cli to 2.3.0-beta.2

--- blueprints/ember-cli-blueprint-test-helpers/index.js
+++ blueprints/ember-cli-blueprint-test-helpers/index.js
@@ -5,11 +5,12 @@ module.exports = {
   normalizeEntityName: function(){},
   afterInstall: function() {
     return Promise.all([
+      this.insertIntoFile('./.npmignore', 'node-tests/'),
       this.addPackageToProject('ember-cli-internal-test-helpers', '^0.6.0'),
-      this.addPackageToProject('glob', '5.0.13'),
-      this.addPackageToProject('mocha', '^2.2.1'),
-      this.addPackageToProject('mocha-only-detector', '0.0.2'),
-      this.addPackageToProject('rimraf', '2.3.2')
+      // this.addPackageToProject('glob', '5.0.13'),
+      // this.addPackageToProject('mocha', '^2.2.1'),
+      // this.addPackageToProject('mocha-only-detector', '0.0.2'),
+      // this.addPackageToProject('rimraf', '2.3.2')
     ]);
   }
 };
