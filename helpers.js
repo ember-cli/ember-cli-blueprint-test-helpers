3d8dfb58ab70587bf8e9e287927aa9e65fff401d
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-04-04 19:33:35 +0200

    Add emberGenerateDestroy() function

--- helpers.js
+++ helpers.js
@@ -2,6 +2,7 @@ module.exports = {
   emberNew: require('./lib/ember-new'),
   emberGenerate: require('./lib/ember-generate'),
   emberDestroy: require('./lib/ember-destroy'),
+  emberGenerateDestroy: require('./lib/ember-generate-destroy'),
   modifyPackages: require('./lib/modify-packages'),
   setupPodConfig: require('./lib/setup-pod-config'),
 };
