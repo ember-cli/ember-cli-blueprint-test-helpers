b8921c0253cd6652bb30519de4cfcc6a290d4ae0
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-04-04 20:24:18 +0200

    blueprints: Adjust "blueprint-test" blueprint to new API

--- blueprints/blueprint-test/files/__root__/__name__-test.js
+++ blueprints/blueprint-test/files/__root__/__name__-test.js
@@ -1,20 +1,22 @@
 'use strict';
 
-var setupTestHooks     = require('ember-cli-blueprint-test-helpers/lib/helpers/setup');
-var BlueprintHelpers   = require('ember-cli-blueprint-test-helpers/lib/helpers/blueprint-helper');
-var generateAndDestroy = BlueprintHelpers.generateAndDestroy;
+var blueprintHelpers = require('ember-cli-blueprint-test-helpers/helpers');
+var setupTestHooks = blueprintHelpers.setupTestHooks;
+var emberNew = blueprintHelpers.emberNew;
+var emberGenerateDestroy = blueprintHelpers.emberGenerateDestroy;
+
+var expect = require('ember-cli-blueprint-test-helpers/chai').expect;
 
 describe('Acceptance: ember generate and destroy <%= blueprintName %>', function() {
   setupTestHooks(this);
-  
-  it('<%= blueprintName %> foo', function() {
-    // pass any additional command line options in the arguments array
-    return generateAndDestroy(['<%= blueprintName %>', 'foo'], {
-      // define files to assert, and their contents
-      files: [
-        // { file: 'app/type/foo.js', contains: ['foo']}
-      ]
-    });
-  });
 
+  it('<%= blueprintName %> foo', function() {
+    var args = ['<%= blueprintName %>', 'foo'];
+
+    // pass any additional command line options in the arguments array
+    return emberNew()
+      .then(() => emberGenerateDestroy(args, (file) => {
+        // expect(file('app/type/foo.js)).to.contain('foo');
+    }));
+  });
 });
