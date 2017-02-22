3e5f5299a68f20ebd8018d195d28bfa36e032804
Kelly Selden <kellyselden@gmail.com>
2017-02-21 22:10:55 -0800

    add disableMocking option

--- node-tests/blueprints/ember-cli-blueprint-test-helpers-test.js
+++ node-tests/blueprints/ember-cli-blueprint-test-helpers-test.js
@@ -10,39 +10,66 @@ var stripAnsi = require('strip-ansi');
 var expect = require('../../chai').expect;
 var file = require('../../chai').file;
 
+function getDevDependencies() {
+  let pkg = fs.readJsonSync('package.json');
+  return Object.keys(pkg.devDependencies);
+}
+
 describe('Acceptance: ember generate ember-cli-blueprint-test-helpers', function() {
-  setupTestHooks(this);
+  describe('without mocking', function() {
+    setupTestHooks(this);
 
-  it('ember-cli-blueprint-test-helpers', function() {
-    var args = ['ember-cli-blueprint-test-helpers'];
+    it('ember-cli-blueprint-test-helpers', function() {
+      var args = ['ember-cli-blueprint-test-helpers'];
 
-    // pass any additional command line options in the arguments array
-    return emberNew()
-      .then(() => emberGenerate(args))
-      .then((result) => {
-        // test ui output because we can't test for actual npm install modifying package.json
-        expect(stripAnsi(result.outputStream.join(' ')))
-          .to.contain('install package mocha');
-        expect(file('.npmignore')).to.contain('node-tests/');
-      });
+      // pass any additional command line options in the arguments array
+      return emberNew()
+        .then(() => emberGenerate(args))
+        .then((result) => {
+          // test ui output because we can't test for actual npm install modifying package.json
+          expect(stripAnsi(result.outputStream.join(' ')))
+            .to.contain('install package mocha');
+          expect(file('.npmignore')).to.contain('node-tests/');
+        });
+    });
+
+    it('doesn\'t write to existing nodetest scripts', function() {
+      var args = ['ember-cli-blueprint-test-helpers'];
+
+      // pass any additional command line options in the arguments array
+      return emberNew()
+        .then(() => {
+          // modify package
+          let pkg = fs.readJsonSync('package.json');
+          pkg.scripts.nodetest = 'mocha tests';
+          fs.writeJsonSync('package.json', pkg, {spaces:2});
+        })
+        .then(() => emberGenerate(args))
+        .then((result) => {
+          let output = result.outputStream.join('');
+          expect(output).to.contain('Could not update "nodetest" script in package.json. Please add "mocha node-tests --recursive" to your nodetest script.');
+          expect(file('package.json')).to.contain('mocha tests');
+        });
+    });
   });
 
-  it('doesn\'t write to existing nodetest scripts', function() {
-    var args = ['ember-cli-blueprint-test-helpers'];
+  describe('with mocking', function() {
+    setupTestHooks(this, {
+      disableMocking: true,
+      timeout: 300000
+    });
 
-    // pass any additional command line options in the arguments array
-    return emberNew()
-      .then(() => {
-        // modify package
-        let pkg = fs.readJsonSync('package.json');
-        pkg.scripts.nodetest = 'mocha tests';
-        fs.writeJsonSync('package.json', pkg, {spaces:2});
-      })
-      .then(() => emberGenerate(args))
-      .then((result) => {
-        let output = result.outputStream.join('');
-        expect(output).to.contain('Could not update "nodetest" script in package.json. Please add "mocha node-tests --recursive" to your nodetest script.');
-        expect(file('package.json')).to.contain('mocha tests');
-      });
+    it('adds mocha as a dependency', function() {
+      var args = ['ember-cli-blueprint-test-helpers'];
+
+      return emberNew()
+        .then(() => {
+          expect(getDevDependencies()).to.not.contain('mocha');
+        })
+        .then(() => emberGenerate(args))
+        .then(() => {
+          expect(getDevDependencies()).to.contain('mocha');
+        });
+    });
   });
 });
