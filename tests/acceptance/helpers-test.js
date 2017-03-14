1da201023ca915a775229cf2da1fd73b378751b9
simonihmig <ihmig@kaliber5.de>
2017-03-14 09:21:37 +0100

    Cleanup symlinked addon after tests

--- tests/acceptance/helpers-test.js
+++ tests/acceptance/helpers-test.js
@@ -3,9 +3,11 @@ var setupTestHooks = require('../../lib/helpers/setup');
 var blueprintHelpers = require('../../helpers');
 var emberNew = blueprintHelpers.emberNew;
 var emberGenerateDestroy = blueprintHelpers.emberGenerateDestroy;
+var path = require('path');
 
 var chai = require('../../chai');
 var expect = chai.expect;
+var dir = chai.dir;
 
 describe('Acceptance: helpers', function() {
   setupTestHooks(this);
@@ -17,6 +19,21 @@ describe('Acceptance: helpers', function() {
             .to.contain('return emberNew()')
             .to.contain('.then(() => emberGenerateDestroy(args, (file) => {');
         }));
+    });
+  });
+
+  describe('setupTestHooks', function() {
+    var symlink = path.resolve(__dirname, '..', '..', 'node_modules', 'ember-cli-blueprint-test-helpers');
+
+    after(function() {
+      expect(dir(symlink)).to.not.exist;
+    });
+
+    it('cleans up symlinked addon', function() {
+      return emberNew()
+        .then(() => {
+          expect(dir(symlink)).to.exist;
+        });
     });
   });
 });
