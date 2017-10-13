1d5de86abdf3f31d7cb3176210573fe06e38b461
Beatriz Magalhães <mbeatrizmagalhaes@gmail.com>
2017-10-13 23:19:51 +0100

    use strict on helper-tests

--- tests/acceptance/helpers-test.js
+++ tests/acceptance/helpers-test.js
@@ -1,3 +1,4 @@
+'use strict';
 const setupTestHooks = require('../../lib/helpers/setup');
 
 const blueprintHelpers = require('../../helpers');
--- tests/lint-test.js
+++ tests/lint-test.js
@@ -2,7 +2,7 @@
 
 const lint = require('mocha-eslint');
 
-var paths = [
+let paths = [
   'blueprints',
   'lib',
   'node-tests',
