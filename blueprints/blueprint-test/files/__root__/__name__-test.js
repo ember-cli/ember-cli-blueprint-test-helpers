d9741cf57bf668359e43ec0ec64de9dd1e15097f
Joe Vu <joe.vu@homeslicesolutions.com>
2019-04-12 02:12:13 -0700

    Fix Docs and modernize test (#231)
    Fix Docs and modernize test

--- blueprints/blueprint-test/files/__root__/__name__-test.js
+++ blueprints/blueprint-test/files/__root__/__name__-test.js
@@ -1,11 +1,14 @@
 'use strict';
 
-const blueprintHelpers = require('ember-cli-blueprint-test-helpers/helpers');
-const setupTestHooks = blueprintHelpers.setupTestHooks;
-const emberNew = blueprintHelpers.emberNew;
-const emberGenerateDestroy = blueprintHelpers.emberGenerateDestroy;
+const { 
+  setupTestHooks,
+  emberNew,
+  emberGenerateDestroy,
+} = require('ember-cli-blueprint-test-helpers/helpers');
 
-const expect = require('ember-cli-blueprint-test-helpers/chai').expect;
+const {
+  expect
+} = require('ember-cli-blueprint-test-helpers/chai');
 
 describe('Acceptance: ember generate and destroy <%= blueprintName %>', function() {
   setupTestHooks(this);
@@ -15,7 +18,7 @@ describe('Acceptance: ember generate and destroy <%= blueprintName %>', function
 
     // pass any additional command line options in the arguments array
     return emberNew(<%= muOption %>)
-      .then(() => emberGenerateDestroy(args, (file) => {
+      .then(() => emberGenerateDestroy(args, (/* file */) => {
         // expect(file('app/type/foo.js')).to.contain('foo');
     }));
   });
