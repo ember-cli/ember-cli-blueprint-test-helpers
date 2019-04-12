c41d1687f9896bda101519bb2f2bf492932519db
Joe <joe.vu@homeslicesolutions.com>
2019-04-11 22:25:58 -0700

    PR fixes

--- blueprints/blueprint-test/files/__root__/__name__-test.js
+++ blueprints/blueprint-test/files/__root__/__name__-test.js
@@ -14,11 +14,11 @@ describe('Acceptance: ember generate and destroy <%= blueprintName %>', function
   setupTestHooks(this);
 
   it('<%= blueprintName %> foo', function() {
-    const args = ['<%= blueprintName %>', 'foo'];
+    let args = ['<%= blueprintName %>', 'foo'];
 
     // pass any additional command line options in the arguments array
     return emberNew(<%= muOption %>)
-      .then(() => emberGenerateDestroy(args, () => {
+      .then(() => emberGenerateDestroy(args, (/* file */) => {
         // expect(file('app/type/foo.js')).to.contain('foo');
     }));
   });
